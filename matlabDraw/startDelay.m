d1ac164a13c6c1bcda6399b678b1e780aec50e26
nhzc123 <charles.nhzc@gmail.com>
2016-04-07 21:05:05 +0800

    matlabDraw

--- matlabDraw/startDelay.m
+++ matlabDraw/startDelay.m
@@ -1,1 +1,27 @@
-[startDelayMartix] = textread('/Users/user/Downloads/startDelay', '%f');
+hFig = figure(1);
+set(gcf, 'PaperPositionMode','auto');
+set(hFig, 'Position', [0 0 800 600])
+ftsz = 16;
+CDFPoint = 100;
+startLine = 0;
+endLine = 10;
+[startDelayMartix] = textread('/Users/user/Downloads/graduateData/startDelay', '%f');
+[xAxis, yAxis] = funcCDF(CDFPoint, startLine, endLine, startDelayMartix);
+plot(xAxis, yAxis, 'x-', 'LineWidth', 1, 'MarkerSize', 5,'Color','r');
+hold on;
+[startDelayOtt] = textread('/Users/user/Downloads/graduateData/startDelayOtt', '%f');
+[xOttAxis, yOttAxis] = funcCDF(CDFPoint, startLine, endLine, startDelayOtt);
+plot(xOttAxis, yOttAxis, 's-', 'LineWidth', 1, 'MarkerSize', 5,'Color','b');
+hold on;
+[startDelayIPad] = textread('/Users/user/Downloads/graduateData/startDelayIPad', '%f');
+[xIPadAxis, yIPadAxis] = funcCDF(CDFPoint, startLine, endLine, startDelayIPad);
+plot(xIPadAxis, yIPadAxis, 'd-', 'LineWidth', 1, 'MarkerSize', 5,'Color','k');
+hold on;
+
+xlabel('start delay(second)','fontsize',ftsz);
+ylabel('cumulative distribution function', 'fontsize', ftsz);
+hleg = legend('total','Ott','Ipad', 'Location','NorthWest');
+set(hleg,'FontSize',20);
+grid on;
+print -deps2c startDelay.eps;
+
